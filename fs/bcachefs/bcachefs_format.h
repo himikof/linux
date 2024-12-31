cd5721bdc56ab54cf6d5007071b9ffe108015060
Kent Overstreet <kent.overstreet@linux.dev>
2024-12-30 21:29:49 -0500

    bcachefs: BCH_SB_LAST_FSCK_VERSION()
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/bcachefs_format.h
+++ fs/bcachefs/bcachefs_format.h
@@ -870,6 +870,7 @@ LE64_BITMASK(BCH_SB_CSUM_ERR_RETRY_NR,	struct bch_sb, flags[6], 14, 20);
 LE64_BITMASK(BCH_SB_DEGRADED_ACTION,	struct bch_sb, flags[6], 20, 22);
 LE64_BITMASK(BCH_SB_CASEFOLD,		struct bch_sb, flags[6], 22, 24);
 LE64_BITMASK(BCH_SB_IO_ERROR_THRESHOLD,	struct bch_sb, flags[6], 24, 40);
+LE64_BITMASK(BCH_SB_LAST_FSCK_VERSION,	struct bch_sb, flags[6], 40, 56);
 
 static inline __u64 BCH_SB_COMPRESSION_TYPE(const struct bch_sb *sb)
 {
