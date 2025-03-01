d600fca68da38816c074fc9ead3b3f27945816bf
Kent Overstreet <kent.overstreet@linux.dev>
2025-03-01 15:51:59 -0500

    bcachefs: Implement freeze/thaw
    This fills out our blk_holders_ops with freeze and thaw callbacks, for
    shutting down IO (generally during a system suspend).
    
    This is implemented completely differently as on other filesystems
    since we have a low level synchronization object which conveniently
    works well for us - bch_dev.io_ref, normally used for guarding against a
    device being offlined while in use.
    
    bch2_dev_get_ioref() now checks if a freeze is in progress if it fails
    to get ca->io_ref, and sleeps until complete and ca->io_ref is alive.
    
    We also need a bit of synchronization for freeze/suspend vs. device
    online/offline, done with the new bch_dev.io_ref_statechange_lock.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb-members.h
+++ fs/bcachefs/sb-members.h
@@ -294,26 +294,7 @@ static inline struct bch_dev *bch2_dev_iterate(struct bch_fs *c, struct bch_dev 
 	return bch2_dev_tryget(c, dev_idx);
 }
 
-static inline struct bch_dev *bch2_dev_get_ioref(struct bch_fs *c, unsigned dev,
-						 int rw, unsigned ref_idx)
-{
-	might_sleep();
-
-	rcu_read_lock();
-	struct bch_dev *ca = bch2_dev_rcu(c, dev);
-	if (ca && !enumerated_ref_tryget(&ca->io_ref[rw], ref_idx))
-		ca = NULL;
-	rcu_read_unlock();
-
-	if (ca &&
-	    (ca->mi.state == BCH_MEMBER_STATE_rw ||
-	    (ca->mi.state == BCH_MEMBER_STATE_ro && rw == READ)))
-		return ca;
-
-	if (ca)
-		enumerated_ref_put(&ca->io_ref[rw], ref_idx);
-	return NULL;
-}
+struct bch_dev *bch2_dev_get_ioref(struct bch_fs *, unsigned, int, unsigned);
 
 extern const struct bch_sb_field_ops bch_sb_field_ops_members_v1;
 extern const struct bch_sb_field_ops bch_sb_field_ops_members_v2;
