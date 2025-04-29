koverstreet/bcachefs

bcachefs: BCH_FS_fsck_verify_pass
Bail out immediately on error in the fsck verify pass.

Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/bcachefs/error.c
@@ -315,10 +315,14 @@ int __bch2_fsck_err(struct bch_fs *c,
 		prt_printf(out, bch2_log_msg(c, ""));
 #endif
 
-	if ((flags & FSCK_CAN_FIX) &&
-	    (flags & FSCK_AUTOFIX) &&
-	    (c->opts.errors == BCH_ON_ERROR_continue ||
-	     c->opts.errors == BCH_ON_ERROR_fix_safe)) {
+	if (test_bit(BCH_FS_fsck_verify_pass, &c->flags)) {
+		prt_str(out, ", shutting down");
+		inconsistent = true;
+		ret = -BCH_ERR_fsck_errors_not_fixed;
+	} else  if ((flags & FSCK_CAN_FIX) &&
+		    (flags & FSCK_AUTOFIX) &&
+		    (c->opts.errors == BCH_ON_ERROR_continue ||
+		     c->opts.errors == BCH_ON_ERROR_fix_safe)) {
 		prt_str(out, ", ");
 		prt_actioning(out, action);
 		ret = -BCH_ERR_fsck_fix;

himikof/linux

fs/bcachefs/error.c
@@ -510,9 +510,13 @@ int __bch2_fsck_err(struct bch_fs *c,
 		goto err_unlock;
 	}
 
-	if ((flags & FSCK_AUTOFIX) &&
-	    (c->opts.errors == BCH_ON_ERROR_continue ||
-	     c->opts.errors == BCH_ON_ERROR_fix_safe)) {
+	if (test_bit(BCH_FS_fsck_verify_pass, &c->flags)) {
+		prt_str(out, ", shutting down");
+		inconsistent = true;
+		ret = -BCH_ERR_fsck_errors_not_fixed;
+	} else if ((flags & FSCK_AUTOFIX) &&
+		   (c->opts.errors == BCH_ON_ERROR_continue ||
+		    c->opts.errors == BCH_ON_ERROR_fix_safe)) {
 		prt_str(out, ", ");
 		if (flags & FSCK_CAN_FIX) {
 			prt_actioning(out, action);