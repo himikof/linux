koverstreet/bcachefs

bcachefs: Run check_allocations() in rw mode
Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/bcachefs/recovery_passes_types.h
@@ -21,11 +21,11 @@
 	x(initialize_subvolumes,		 2, 0)				\
 	x(snapshots_read,			 3, PASS_ALWAYS)		\
 	x(check_allocations_early,		40, PASS_FSCK)			\
-	x(check_allocations,			 5, PASS_FSCK)			\
-	x(trans_mark_dev_sbs,			 6, PASS_ALWAYS|PASS_SILENT)	\
 	x(fs_journal_alloc,			 7, PASS_ALWAYS|PASS_SILENT)	\
 	x(set_may_go_rw,			 8, PASS_ALWAYS|PASS_SILENT)	\
 	x(journal_replay,			 9, PASS_ALWAYS)		\
+	x(check_allocations,			 5, PASS_ONLINE|PASS_FSCK)	\
+	x(trans_mark_dev_sbs,			 6, PASS_ALWAYS|PASS_SILENT)	\
 	x(check_alloc_info,			10, PASS_ONLINE|PASS_FSCK)	\
 	x(check_lrus,				11, PASS_ONLINE|PASS_FSCK)	\
 	x(check_btree_backpointers,		12, PASS_ONLINE|PASS_FSCK)	\
@@ -43,7 +43,7 @@
 	x(fs_upgrade_for_subvolumes,		22, 0)				\
 	x(check_inodes,				24, PASS_FSCK)			\
 	x(check_extents,			25, PASS_FSCK)			\
-	x(check_indirect_extents,		26, PASS_FSCK)			\
+	x(check_indirect_extents,		26, PASS_ONLINE|PASS_FSCK)	\
 	x(check_dirents,			27, PASS_FSCK)			\
 	x(check_xattrs,				28, PASS_FSCK)			\
 	x(check_root,				29, PASS_ONLINE|PASS_FSCK)	\

himikof/linux

fs/bcachefs/recovery_passes_types.h
@@ -30,11 +30,11 @@
 	x(initialize_subvolumes,		 2, 0)					\
 	x(snapshots_read,			 3, PASS_ALWAYS)			\
 	x(check_allocations_early,		42, PASS_FSCK_ALLOC)			\
-	x(check_allocations,			 5, PASS_FSCK_ALLOC)			\
 	x(trans_mark_dev_sbs,			 6, PASS_ALWAYS|PASS_SILENT|PASS_ALLOC)	\
 	x(fs_journal_alloc,			 7, PASS_ALWAYS|PASS_SILENT|PASS_ALLOC)	\
 	x(set_may_go_rw,			 8, PASS_ALWAYS|PASS_SILENT)		\
 	x(journal_replay,			 9, PASS_ALWAYS)			\
+	x(check_allocations,			 5, PASS_ONLINE|PASS_FSCK_ALLOC)	\
 	x(check_alloc_info,			10, PASS_ONLINE|PASS_FSCK_ALLOC)	\
 	x(check_lrus,				11, PASS_ONLINE|PASS_FSCK_ALLOC)	\
 	x(check_btree_backpointers,		12, PASS_ONLINE|PASS_FSCK_ALLOC)	\