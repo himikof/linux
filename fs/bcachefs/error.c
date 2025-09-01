f98252402f20f5fe4cebd66e8c8ede72c8ed791d
Kent Overstreet <kent.overstreet@linux.dev>
2025-09-01 17:46:33 -0400

    fixup! bcachefs: -o fix_errors may now be used without -o fsck

--- fs/bcachefs/error.c
+++ fs/bcachefs/error.c
@@ -394,7 +394,7 @@ int bch2_fsck_err_opt(struct bch_fs *c,
 		flags |= fsck_flags_extra[err];
 
 	if (test_bit(BCH_FS_in_fsck, &c->flags) ||
-	    test_bit(BCH_FS_in_recovery, &c->flags)) {
+	    c->opts.fix_errors != FSCK_FIX_exit) {
 		if (!(flags & (FSCK_CAN_FIX|FSCK_CAN_IGNORE)))
 			return bch_err_throw(c, fsck_repair_unimplemented);
 
